d3d8ac456658a30c74696cc06494666c107149b5
TamarGE <emiliaguerson@gmail.com>
2022-04-04 12:05:17 -0300

    Video 04
    Hice el tutorial de Unity hasta el video  "C# para Unity 04" del TP 02

---
+++ Assets/Scripts/MayorEdad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorEdad : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text MiTexto;
+    void Start()
+    {
+        if (edadUsuario >= 18)
+        {
+            MiTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            MiTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+}
