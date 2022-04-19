e02232920d6fa93d458122d444217a985e229a1a
TamarGE <emiliaguerson@gmail.com>
2022-04-19 11:48:14 -0300

    Videos 4 y 5
    Seguí la consigna de los videos 4 y 5 del TP 02

---
+++ Assets/Scripts/ValidarContra.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContra : MonoBehaviour
+{
+    string contraCorrecta;
+    string contraUsuario;
+    public Text ingresoUsuario;
+    void Start()
+    {
+        contraCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarContraseña()
+    {
+        contraUsuario = ingresoUsuario.text;
+        if (contraUsuario == contraCorrecta)
+        {
+            Debug.Log("Bienvenid@");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
